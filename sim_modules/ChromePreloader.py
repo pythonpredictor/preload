a351e4526ecd497c51703d361a2c07d6c2aa59be
SuWilliam <wwilliam6@gatech.edu>
2017-02-14 02:12:22 -0500

    think fixed module error

--- sim_modules/ChromePreloader.py
+++ sim_modules/ChromePreloader.py
@@ -8,6 +8,8 @@ class ChromePreloader(SimModule):
         self.name = name
         self.module_type = module_type
         self.simulator = simulator
+        self.number_incorrect = 0
+        self.number_correct = 0
 
     def build(self):
         self.simulator.subscribe(EventType.SCREEN, self.preloadNetflix)
@@ -21,8 +23,9 @@ class ChromePreloader(SimModule):
     	# so that it checks if the preload is successful
         self.simulator.broadcast("org.chromium.chrome.browser.document.ChromeLauncherActivity")
 
-        # make an alarm that
-        register_alarm(300000, verify())
+        # make an alarm that goes off in 5 minutes or until something happens
+        alarm = None
+        self.simulatorregister_alarm(alarm, verify)
 
 
         #
@@ -30,3 +33,4 @@ class ChromePreloader(SimModule):
     	# 	print("Preload Successful")
     	# print('This is a test for preload predictor')
     def verify(self):
+        number_correct += 1
--- sim_modules/__init__.py
+++ sim_modules/__init__.py
@@ -4,6 +4,6 @@ from sim_interface import SimModuleType
 def get_simulator_module(module_name, simulator, module_settings):
     # Insert call to create specific simulator module here
     if (module_name == 'chrome-preload-predictor'):
-        return NetflixPreloader(module_name, SimModuleType.CHROME_PRELOAD_PREDICTOR, simulator)
+        return ChromePreloader(module_name, SimModuleType.CHROME_PRELOAD_PREDICTOR, simulator)
     else:
         print("No relative module is created")
