2ff0b1c7f6395c0c839ea43786d88e5939c8a5f7
jiashenC <caojiashen24@gmail.com>
2017-02-24 10:29:04 -0500

    fix logical bugs and add decrease for freq_count

--- sim_modules/preload_predictor.py
+++ sim_modules/preload_predictor.py
@@ -1,6 +1,8 @@
 from sim_interface import SimModule
 from events import EventType, Event
+import device
 import datetime
+from math import exp
 
 
 class Preload(SimModule):
@@ -16,30 +18,31 @@ class Preload(SimModule):
 
     def build(self):
         self.simulator.subscribe(EventType.SCREEN, self.preload)
-        self.simulator.subscribe(EventType.NOTIFICATION, self.verify)
+        self.simulator.subscribe(EventType.APP_ACTIVITY_USAGE, self.verify)
 
     def finish(self):
-        pass
+        print("accuracy: " + str(float(self.correct) / self.total_time))
 
     # method to handle the event type being called
     def preload(self, event):
+        for app in self.freq_count:
+            self.freq_count.update({app: self.freq_count[app] * 0.5 + 1})
         # check Screen On event
-        if event.state.name == "ON" and len(self.freq_count) > 0:
+        if event.state == device.ScreenState.ON and len(self.freq_count) > 0:
             highest_app = max(self.freq_count, key=self.freq_count.get)
+            self.total_time += 1
             self.prediction = (highest_app, event.timestamp)
             self.simulator.broadcast(Event(event.timestamp, EventType.PRELOAD_APP))
 
     # method to verify the preload result
     def verify(self, event):
-        # update freq count dictionary
-        if event.app_id in self.freq_count:
-            self.freq_count.update({event.app_id: self.freq_count[event.app_id] + 1})
-        else:
-            self.freq_count.update({event.app_id: 1})
         # set time margin
         margin = datetime.timedelta(seconds=5 * 3600)
         app_id, timestamp = self.prediction
         if timestamp is not None and event.timestamp - timestamp < margin and event.app_id == app_id:
             self.correct += 1
-        self.total_time += 1
-        print("accuracy: " + str(float(self.correct) / self.total_time))
+        # update freq count dictionary
+        if event.app_id in self.freq_count and timestamp is not None:
+            self.freq_count.update({event.app_id: self.freq_count[event.app_id] + 1})
+        else:
+            self.freq_count.update({event.app_id: 1})
