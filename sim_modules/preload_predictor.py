42d8a6f9eb2d482d004b7360014b9f379f46c564
jiashenC <caojiashen24@gmail.com>
2017-03-02 12:36:13 -0500

    fix some errors in preload

--- sim_modules/preload_predictor.py
+++ sim_modules/preload_predictor.py
@@ -6,7 +6,7 @@ from math import exp
 
 
 class Preload(SimModule):
-    def __init__(self, name, module_type, simulator, module_settings):
+    def __init__(self, name, module_type, simulator):
         self.name = name
         self.module_type = module_type
         self.simulator = simulator
@@ -17,14 +17,10 @@ class Preload(SimModule):
         self.correct = 0
 
         # new variables, thinking about making it similar to freq_count
-
         # list of dictionaries [{morn_freq}, {aftn_freq}, {ngt_freq}]
-
-        #freq_dict_index(time) -> the interval index
-
+        # freq_dict_index(time) -> the interval index
         self.interval_time = 2
-        self.intervals = 24/self.interval_time
-
+        self.intervals = 24 // self.interval_time
         self.time_expon = 1
         self.index = 0
 
@@ -33,33 +29,25 @@ class Preload(SimModule):
         self.simulator.subscribe(EventType.APP_ACTIVITY_USAGE, self.verify)
 
     def finish(self):
-        print("accuracy: " + str(float(self.correct) / self.total_time))
+        print("accuracy: " + str(self.correct / self.total_predictions))
 
     # method to handle the event type being called
     def preload(self, event):
+        # gets the current time and converts that into an index
+        current_hour = event.timestamp.hour
+        self.index = current_hour // self.interval_time
 
-        #gets the current time and converts that into an index
-        current_hour = self.simulator.get_current_time(hour)
-        self.index = current_hour/self.interval_time
-
-        #appends the corresponding number of indicies to the freq_count_list
-        for x in range(intervals):
-            freq_count_list.append({})
-
+        # appends the corresponding number of index to the freq_count_list
+        for x in range(self.intervals):
+            self.freq_count_list.append({})
         for app in self.freq_count_list[self.index]:
             self.freq_count_list[self.index].update({app: self.freq_count_list[self.index][app] * 0.5})
 
-
-
         # check Screen On event and preloads the app that has the highest frequency of usage
-
-        #before preloading the app check to see if it is morning, afternoon or night and then preload the corresponding application
-
-                #for current time : simulator.get_current_time
-
+        # before preloading the app check to see if it is morning, afternoon or night and then preload the
+        # corresponding application for current time : simulator.get_current_time
         # morn[freq, accessed_in_interval]
         # get hour of the time out of this current_time
-
         if event.state == device.ScreenState.ON and len(self.freq_count_list[self.index]) > 0:
             highest_app = max(self.freq_count_list[self.index], key=self.freq_count_list[self.index].get)
             self.total_predictions += 1
@@ -82,7 +70,6 @@ class Preload(SimModule):
         else:
             self.freq_count_list[self.index].update({event.app_id: 1})
 
-        #
-
-    # for each app event in the frequency table, need to add a time_expon factor where it is decremented at an logorithmic rate
-    # when verify is called every app in the corresponding freq_count table will be logged except for the app that is called
+        # for each app event in the frequency table, need to add a time_expon factor where it is decremented
+        # at an logorithmic rate when verify is called every app in the corresponding freq_count table will
+        # be logged except for the app that is called
